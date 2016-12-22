cd2d09c86f2307fd5e75405591afe7a28824218d
jrexia <jrexia@163.com>
2016-12-22 13:11:06 +0800

    最新版本
    最新版本

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 $(function() {
     // alert("ddfd");
     $('#header').css("height", window.innerHeight + "px");
+
+    $("#part2").waypoint(function() {
+        $("#right1").addClass("img1");
+        $("#right2").addClass("img2");
+        $("#right3").addClass("img3");
+    }, { offset: 250 });
     // $('#div2').css("height", window.innerHeight + 1 + "px");
     // $('#div3').css("height", window.innerHeight + 1 + "px");
     // var a = window.innerHeight;
@@ -15,7 +21,7 @@ $(function() {
     //     $('.loading-process').css("height", a + 20 + "px");
     // }, { offset: -1 });
 
-    $('#header .btn-home .img-btn').hover(function() {
+    $('.img').hover(function() {
         $(this).css('height', '125');
         $(this).css('width', '140');
         $(this).css("clip", "rect(0px,60px,70px,0px)");
@@ -24,6 +30,18 @@ $(function() {
         $(this).css('width', '110');
         $(this).css("clip", "rect(0px,46px,59px,0px)");
     });
+
+    $('#part2 .btn-about .img-about').hover(function() {
+        $(this).css('height', '125');
+        $(this).css('width', '140');
+        // $(this).animate({ clip: 'rect(69px,62px,134px,0px)' }, 3000);
+        $(this).css("clip", "rect(69px,62px,134px,0px)");
+    }, function() {
+        $(this).css('height', '100');
+        $(this).css('width', '110');
+        $(this).css("clip", "rect(59px,46px,118px,0px)");
+    });
+
 
     // 滚动翻页部分开始
     $(".panel").css({ "height": $(window).height() });
@@ -55,4 +73,9 @@ $(function() {
             }
         })
         // 导航随鼠标滚动吸顶结束
+        // 圆形按钮点击缓慢进入下一个页面开始
+    $(".btn").click(function() {
+        $("html,body").animate({ scrollTop: $(window).scrollTop() + $(window).height() }, "slow");
+    });
+    // 圆形按钮点击缓慢进入下一个页面结束
 });
